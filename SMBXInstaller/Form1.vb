d2b5a63c8b8416b8fc666c15866992448267e3bf
Mike Santiago <miketheripper1@msn.com>
2013-11-11 16:14:45 -0500

    Adds All Files
    True Initial Commit

---
+++ SMBXInstaller/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System
+Imports System.IO
+
+Public Class Form1
+    Dim INI_File As New IniFile(Environment.CurrentDirectory + "\worldIndex.ini")
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim oForm As New Settings()
+        oForm.Show()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If My.Settings.isFirstRun = True Then
+            MsgBox("Hi! I see this is your first run!\nPlease go to Settings and configure your SMBX directories")
+            UpdateRepo()
+
+            My.Settings.isFirstRun = False
+
+        ElseIf My.Settings.isFirstRun = False Then
+            UpdateRepo()
+        End If
+
+
+
+
+        ListBox2.DataSource = Directory.GetDirectories(My.Settings.worldlocation.ToString)
+
+        Dim SelectWorld As String = CStr(ListBox2.SelectedItem)
+
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Public Sub UpdateRepo()
+        Dim appdir As String = Environment.CurrentDirectory
+        'https://dl.dropboxusercontent.com/u/62304851/worldIndex.ini
+        MsgBox("Updating Repo", MsgBoxStyle.SystemModal)
+        If My.Computer.FileSystem.FileExists(appdir + "\worldIndex.ini") Then
+            My.Computer.FileSystem.DeleteFile(appdir + "\worldIndex.ini")
+        End If
+        'If My.Computer.Network.Ping("https://dl.dropboxusercontent.com") Then
+        My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/u/62304851/worldIndex.ini", appdir + "\worldIndex.ini")
+
+        'End If
+
+    End Sub
+End Class
---
+++ SMBXInstaller/Settings.vb
@@ -0,0 +1,40 @@
+﻿Public Class Settings
+
+    Private Sub Settings_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.smbxpath
+        TextBox2.Text = My.Settings.worldlocation
+        TextBox3.Text = My.Settings.executableloc
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Changing SMBX Path from " + My.Settings.smbxpath + " to " + TextBox1.Text)
+        MsgBox("Changing World Location from " + My.Settings.worldlocation + " to " + TextBox2.Text)
+        MsgBox("Changing SMBX Path from " + My.Settings.executableloc + " to " + TextBox3.Text)
+
+        If TextBox1.Text = My.Settings.smbxpath Then
+
+        Else
+            My.Settings.smbxpath = TextBox1.Text
+        End If
+
+        If TextBox2.Text = My.Settings.smbxpath Then
+        Else
+            My.Settings.worldlocation = TextBox2.Text
+        End If
+
+        If TextBox3.Text = My.Settings.smbxpath Then
+        Else
+
+            My.Settings.executableloc = TextBox3.Text
+        End If
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+End Class
